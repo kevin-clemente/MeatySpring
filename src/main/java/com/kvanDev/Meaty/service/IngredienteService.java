02f3a2609334420c9a1f72a42a7936c7cd4d3e08
kevin-clemente <kevinclemente7@gmail.com>
2020-05-26 14:14:16 +0100

    "change properties and service Ingrediente"

--- src/main/java/com/kvanDev/Meaty/service/IngredienteService.java
+++ src/main/java/com/kvanDev/Meaty/service/IngredienteService.java
@@ -1,12 +1,9 @@
 package com.kvanDev.Meaty.service;
 
-
 import com.kvanDev.Meaty.dao.IngreditenteRepository;
 import com.kvanDev.Meaty.model.Ingrediente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
