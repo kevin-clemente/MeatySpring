e37c9d4fa2346d248c77b030b424a61ec547f6a1
kevinclemente <kevinclemente7@gmail.com>
2025-10-20 00:49:26 +0100

    create a unitTest for the Ingrediente service

--- src/test/java/com/kvanDev/Meaty/MeatyApplicationTests.java
+++ src/test/java/com/kvanDev/Meaty/MeatyApplicationTests.java
@@ -1,13 +1,46 @@
 package com.kvanDev.Meaty;
 
+import com.kvanDev.Meaty.api.IngredientController;
+import com.kvanDev.Meaty.model.Ingredient;
+import com.kvanDev.Meaty.model.Type;
+import com.kvanDev.Meaty.service.IngredientsService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import javax.net.ssl.SSLEngineResult;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(IngredientController.class)
 class MeatyApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    IngredientsService ingredientsService;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+        Type type = new Type(1l,"carne");
+        when(ingredientsService.getAllIngredients()).thenReturn(
+                List.of(new Ingredient(1L, "Picanha", type),
+                        new Ingredient(1L, "Maminha", type)
+                        ));
+        this.mockMvc.perform(get("/ingredientes/all"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Maminha")));
 	}
 
 }
